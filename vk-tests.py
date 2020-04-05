a718bdecf4fec9e9ba4a540c969bd7c79ccd31cd
aleroza <48179175+aleroza@users.noreply.github.com>
2020-04-05 19:49:56 +0300

    Update vk-tests.py
    • Фикс глобальности переменных
    • Приведение кода к стандартам всяких пепов
    • Фикс неправильного отображения имени (приложение "Тесты" не может в вывод восточноазиатских символов)
    • Теперь вывод в табличка-like стиле (которая ломается, если попадается имя из прошлого пункта данного ченджлога)

--- vk-tests.py
+++ vk-tests.py
@@ -1,127 +1,154 @@
 import vk
 
-v = 5.103 # Версия API VK
-APP_ID =  # <—— Номер приложения разработчика
-GROUP_ID = 43340456  # <—— Айдишник группы
-TOKEN = ""  # <—— Токен доступа группы
-ADMIN_ID =  # <—— Айдишник админа, которому присылаются ответы
-DATA = []
-offset = 0 # Кол-во обработанных сообщений. По нему определяется сдвиг для нового запроса сообщений
+# v -- версия API VK
+#
+# APP_ID -- номер приложения разработчика (меню "Управление")
+# GROUP_ID -- айдишник группы
+# TOKEN -- токен доступа группы
+# ADMIN_ID -- айдишник админа, которому присылаются ответы
+
+v = 5.103
+APP_ID = 0
+GROUP_ID = 43340456
+TOKEN = ""
+ADMIN_ID = 0
 
 
 def main():
-    global TARGET_Q
+    data = []
+    offset = 0
+    # offset представляет кол-во обработанных сообщений. По нему определяется сдвиг для нового запроса сообщений
 
     print("Логинимся")
-    __login__()
+    vkapi = login()
     print("Получаем кол-во сообщений")
-    __numOfMsgs__()
+    nums = taking_num_of_msgs(vkapi)
     print("Пример вопроса для ввода...")
     print("Q: У него в сумке лежали учебники для сегодняшних уроков, обед и...")
-    TARGET_Q = str(input("Введите любой вопрос из теста, который нужно проверить: "))
+    target_q = str(input("Введите любой вопрос из теста, который нужно проверить: "))
     print("Вытаскиваем ворох сообщений")
-    __takingMsgs__(iter20Num, 20)
-    __takingMsgs__(1, plusIter20Num)
-    print("Ответов на тест --", len(DATA))
+    data, offset = taking_msgs(vkapi, nums["iter_num"], 20, offset, data, target_q)
+    data, offset = taking_msgs(vkapi, 1, nums["plus_iter_num"], offset, data, target_q)
+    print("Ответов на тест --", len(data))
     print("Сортируем результаты")
-    __checkingResults__()
+    data = checking_results(data)
     print("Итоги")
-    print(DATA)
-    print("Ответов после обработки --", len(DATA))
+    print(data)
+    print("Ответов после обработки --", len(data))
 
     print("Сохраняем в файл")
-    __save__()
+    save_to_file(data)
 
 
-
-def __login__():
-    global vkapi
+def login():
     session = vk.Session(access_token=TOKEN)
-    vkapi = vk.API(session, v=v)
+    return vk.API(session, v=v)
 
 
-def __numOfMsgs__():
-    global iter20Num
-    global plusIter20Num
-    numOfMsgs = vkapi.messages.getHistory(group_id=GROUP_ID, start_message_id=-1, peer_id=ADMIN_ID, user_id=ADMIN_ID,
-                                          count=1,
-                                          offset=0)
-    iter20Num = numOfMsgs["count"] // 20
-    plusIter20Num = numOfMsgs["count"] % 20
+def taking_num_of_msgs(vkapi):
+    num_of_msgs = vkapi.messages.getHistory(group_id=GROUP_ID, start_message_id=-1, peer_id=ADMIN_ID, user_id=ADMIN_ID,
+                                            count=1,
+                                            offset=0)
+    iter_num = num_of_msgs["count"] // 20
+    plus_iter_num = num_of_msgs["count"] % 20
+    return {"iter_num": iter_num, "plus_iter_num": plus_iter_num}
 
 
-def __takingMsgs__(iterNum, count):
-    global offset
-    global DATA
-
-    for i in range(iterNum):
+def taking_msgs(vkapi, iter_num, count, offset, data, target_q):
+    for i in range(iter_num):
         messages = vkapi.messages.getHistory(group_id=GROUP_ID, start_message_id=-1, peer_id=ADMIN_ID,
                                              user_id=ADMIN_ID,
                                              count=count,
                                              offset=offset)
-        msgItems = messages["items"]
+        msg_items = messages["items"]
 
         k = 0
-        while k <= count-1: # Определяем индексы начала и конца нужных нам данных, делаем срезы
-            msgItem = msgItems[k]
-            msg = msgItem["text"]
+        # Определяем индексы начала и конца нужных нам данных, делаем срезы
+        while k <= count - 1:
+            msg_item = msg_items[k]
+            msg = msg_item["text"]
 
-            nameStartIndex = 20
-            nameEndIndex = msg.find(" vk.com/")
-            idStartIndex = msg.find("/id")
-            idEndIndex = msg.find("Диалог") - 1
-            scoreStartIndex = msg.find("Набрано баллов ") + 15
-            scoreEndIndex = msg.find("Q: ") - 2
+            name_start_index = 20
+            name_end_index = msg.find(" vk.com/")
+            id_start_index = msg.find("/id")
+            id_end_index = msg.find("Диалог") - 1
+            score_start_index = msg.find("Набрано баллов ") + 15
+            score_end_index = msg.find("Q: ") - 2
 
-            name = msg[nameStartIndex:nameEndIndex]
-            id = msg[idStartIndex:idEndIndex]
-            score = msg[scoreStartIndex:scoreEndIndex]
-            if TARGET_Q in msg:
-                DATA.append([name, id, score])
+            name = msg[name_start_index:name_end_index]
+            id_ = msg[id_start_index:id_end_index]
+            score = msg[score_start_index:score_end_index]
+            if target_q in msg:
+                data.append([name, id_, score])
             k += 1
         offset += count
+    return data, offset
 
 
-def __checkingResults__():
-    global DATA
-    i=0
-    DATA=sorted(DATA, key=lambda idsort: idsort[1]) # Сортируем по возрастанию айдишников
+def checking_results(data):
+    i = 0
+    # Сортируем по возрастанию айдишников
+    data = sorted(data, key=lambda idsort: idsort[1])
     # Удаляем тех, кто перепроходил тест
-    while i + 1 < len(DATA):
+    while i + 1 < len(data):
         k = 1
-        shitNum = 0
-        compOrig = DATA[i]
-        while k < len(DATA):
-            if i + 1 + shitNum<len(DATA):
-                compComp = DATA[i + 1 + shitNum]
-            else: break
-            if compOrig[1] == compComp[1]:
-                shitNum += 1
+        shit_num = 0
+        comp_orig = data[i]
+        while k < len(data):
+            if i + 1 + shit_num < len(data):
+                comp_comp = data[i + 1 + shit_num]
+            else:
+                break
+            if comp_orig[1] == comp_comp[1]:
+                shit_num += 1
             else:
                 break
             k += 1
-        if shitNum > 0:
-            del (DATA[i:i+shitNum + 1])
+        if shit_num > 0:
+            del (data[i:i + shit_num + 1])
         else:
             i += 1
 
     # Удаляем набравших 0 баллов
     i = 0
-    while i + 1 < len(DATA):
-        resultText=DATA[i]
-        resultText=resultText[2]
-        if  resultText[0]=="0":
-            del DATA[i]
-        else: i+=1
-    DATA = sorted(DATA, key=lambda idsort: idsort[2], reverse=True) #Сортируем по убыванию баллов
+    while i + 1 < len(data):
+        result_text = data[i]
+        result_text = result_text[2]
+        if result_text[0] == "0":
+            del data[i]
+        else:
+            i += 1
+    # Сортируем по убыванию баллов
+    data = sorted(data, key=lambda idsort: idsort[2], reverse=True)
 
-def __save__():
+    # Фиксим баги кодировки (приложение "Тесты" не может адекватно выводить восточноазиатские символы)
+    i = 0
+    while i < len(data):
+        name = data[i]
+        if "\\u" in name[0]:
+            name[0] = name[0].replace("\\/", "/").encode().decode("unicode_escape")
+            data[i] = name
+        i += 1
+    return data
+
+
+def save_to_file(data):
     file = open("test-results.txt", "w")
-    for item in DATA:
-        file.write("\n|")
-        for subItem in item:
-            file.write(subItem + " | ")
+    for item in data:
+        k = 1
+        score_len = 6
+        for sub_item in item:
+            if k == 1:
+                file.write("| " + sub_item.ljust(25, " ") + " | ")
+            elif k == 2:
+                file.write(sub_item.ljust(13, " ") + " | ")
+            else:
+                if len(sub_item) > 6:
+                    score_len = 12
+                file.write(sub_item.ljust(score_len, " ") + " |\n")
+            k += 1
     file.close()
+
 
 if __name__ == "__main__":
     main()
